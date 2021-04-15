05d715ea0b9ebde9f6b7608059ed35f4ea6b3a75
perrito11 <49883692+perrito11@users.noreply.github.com>
2021-04-15 08:03:35 -0500

    Actualizacion 8am

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,7 @@
 const section = document.querySelector('#contenedor');
+const listadod = document.querySelector('#listadod');
+const listadot = document.querySelector('#listadot');
+
 
 fetch('http://demo6497253.mockable.io/noticias')
     .then(function(response) {
@@ -28,8 +31,8 @@ function creartarjetas(jsonObj) {
 
         descripcion.textContent = noticias[i].descripcion;
         a.textContent = "Ver mas...";
-
-        //detalle.textContent = noticias[i].detalle;
+        myH2.setAttribute("style", "color:blue")
+            //detalle.textContent = noticias[i].detalle;
 
         //img.src = noticias[i].img;
 
@@ -48,5 +51,117 @@ function creartarjetas(jsonObj) {
 
         section.appendChild(myArticle);
 
+    }
+
+
+
+
+    fetch('http://demo6497253.mockable.io/categoria/deporte')
+        .then(function(response) {
+            return response.json();
+
+        })
+        .then(function(data) {
+            creartarjetas2(data);
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+
+
+
+
+    function creartarjetas2(jsonObj) {
+        const noticias = jsonObj;
+
+        for (var i = 0; i < noticias.length; i++) {
+            const myArticle = document.createElement('div');
+            const myH5 = document.createElement('h5');
+            //const descripcion = document.createElement('p');
+            //const detalle = document.createElement('p');
+            //const img = document.createElement('img');
+            const hr = document.createElement('hr');
+            // const a = document.createElement('a');
+            myH5.textContent = noticias[i].titulo;
+
+            //descripcion.textContent = noticias[i].descripcion;
+            //a.textContent = "Ver mas...";
+
+            //detalle.textContent = noticias[i].detalle;
+
+            //img.src = noticias[i].img;
+
+
+
+
+
+            myArticle.appendChild(myH5);
+
+            //myArticle.appendChild(descripcion);
+            // myArticle.appendChild(detalle);
+            //myArticle.appendChild(img);
+            //myArticle.appendChild(a);
+            myArticle.appendChild(hr);
+
+
+            listadod.appendChild(myArticle);
+
+        }
+
+        fetch('http://demo6497253.mockable.io/categoria/tecnologia')
+            .then(function(response) {
+                return response.json();
+
+            })
+            .then(function(data) {
+                creartarjetas3(data);
+                console.log('data = ', data);
+            })
+            .catch(function(err) {
+                console.error(err);
+            });
+
+
+
+
+        function creartarjetas3(jsonObj) {
+            const noticias = jsonObj;
+
+            for (var i = 0; i < noticias.length; i++) {
+                const myArticle = document.createElement('div');
+                const myH5 = document.createElement('h5');
+                //const descripcion = document.createElement('p');
+                //const detalle = document.createElement('p');
+                //const img = document.createElement('img');
+                const hr = document.createElement('hr');
+                // const a = document.createElement('a');
+                myH5.textContent = noticias[i].titulo;
+
+                //descripcion.textContent = noticias[i].descripcion;
+                //a.textContent = "Ver mas...";
+
+                //detalle.textContent = noticias[i].detalle;
+
+                //img.src = noticias[i].img;
+
+
+
+
+
+                myArticle.appendChild(myH5);
+
+                //myArticle.appendChild(descripcion);
+                // myArticle.appendChild(detalle);
+                //myArticle.appendChild(img);
+                //myArticle.appendChild(a);
+                myArticle.appendChild(hr);
+
+
+                listadot.appendChild(myArticle);
+
+            }
+
+        }
     }
 }
